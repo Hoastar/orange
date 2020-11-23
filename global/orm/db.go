3fa14e277aa1db88822aee0af801a6b2e9658a46
hoastar <soman.hukejie@gmail.com>
2020-11-23 09:59:16 +0800

    global

---
+++ global/orm/db.go
@@ -0,0 +1,15 @@
+/*
+@Time : 2020/10/28 下午11:31
+@Author : hoastar
+@File : db
+@Software: GoLand
+*/
+
+package orm
+
+import "github.com/jinzhu/gorm"
+
+var Eloquent *gorm.DB
+var MysqlConn string
+
+

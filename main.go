532396b9e003733d59ccb9934bf036f37c6aa7a9
hoastar <soman.hukejie@gmail.com>
2020-12-09 22:57:21 +0800

    add main

---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+@Time : 2020/12/9 下午10:53
+@Author : hoastar
+@File : main.go
+@Software: GoLand
+*/
+
+package main
+
+import "github.com/hoastar/orange/cmd"
+
+func main() {
+	cmd.Excute()
+}
+

ab216725d13ebe7f7d566e6399eba83defda3d66
Hoastar <soman.hukejie@gmail.com>
2020-12-17 22:50:28 +0800

    add tpl

--- apis/tpl/tpl.go
+++ apis/tpl/tpl.go
@@ -1,8 +1,17 @@
 /*
-@Time : 2020/12/9 下午11:32 
+@Time : 2020/12/9 下午11:32
 @Author : hoastar
 @File : tpl
 @Software: GoLand
 */
 
 package tpl
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Tpl(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{})
+}

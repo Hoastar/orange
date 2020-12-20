809f7f1a1d4ebc75f914a3d68c97721aa8dcae39
Hoastar <soman.hukejie@gmail.com>
2020-12-20 16:26:15 +0800

    update

--- tools/ip.go
+++ tools/ip.go
@@ -10,34 +10,40 @@ package tools
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"net/http"
 )
 
 func GetLocation(ip string) string {
-	if ip == "127.0.0.1" || ip == "localhost" {
-		return "局域网ip"
+	var address = "已关闭位置获取"
+	if viper.GetBool("settings.public.isLocation") {
+		if ip == "127.0.0.1" || ip == "localhost" {
+			return "局域网ip"
+		}
+
+		// 高地地图接口服务：根据ip确定大致地理位置
+		resp, err := http.Get("https://restapi.amap.com/v3/ip?ip=" + ip + "&key=3fabc36c20379fbb9300c79b19d5d05e")
+		if err != nil {
+			panic(err)
+		}
+
+		defer resp.Body.Close()
+		s, err := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(s))
+
+		m := make(map[string]string)
+
+		err = json.Unmarshal(s, &m)
+		if err != nil {
+			fmt.Println("Umarshal failed", err)
+		}
+
+		if m["province"] == "" {
+			return "位置位置"
+		}
+
+		address = m["province"] + "_" + m["city"]
 	}
-
-	// 高地地图接口服务：根据ip确定大致地理位置
-	resp, err := http.Get("https://restapi.amap.com/v3/ip?ip=" + ip + "&key=3fabc36c20379fbb9300c79b19d5d05e")
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-	s, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(s))
-
-	m := make (map[string]string)
-
-	err = json.Unmarshal(s, &m)
-	if err != nil {
-		fmt.Println("Umarshal failed", err)
-	}
-
-	if m["province"] == "" {
-		return "位置位置"
-	}
-	return m["province"] + "_" + m["city"]
+	return address
 }

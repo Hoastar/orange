2c26d1bc0907dc8d84f4a73bcea98714fe213de3
Hoastar <soman.hukejie@gmail.com>
2020-12-20 16:06:15 +0800

    add ladp

--- pkg/ldap/ldapFieldsMap.go
+++ pkg/ldap/ldapFieldsMap.go
@@ -1,8 +1,43 @@
 /*
-@Time : 2020/12/20 下午3:41 
+@Time : 2020/12/20 下午3:41
 @Author : hoastar
 @File : ldapFieldsMap
 @Software: GoLand
 */
 
 package ldap
+
+import (
+	"github.com/go-ldap/ldap/v3"
+	"github.com/hoastar/orange/models/system"
+)
+
+func LdapFieldsMap(ldapUserInfo *ldap.Entry) (userInfo system.SysUser, err error) {
+	var (
+		ldapFields []map[string]string
+	)
+
+	ldapFields, err = getLdapFields()
+	if err != nil {
+		return
+	}
+
+	for _, v := range ldapFields {
+		switch v["local_field_name"] {
+		case "nick_name":
+			userInfo.NickName = ldapUserInfo.GetAttributeValue(v["ldap_field_name"])
+		case "phone":
+			userInfo.Phone = ldapUserInfo.GetAttributeValue(v["ldap_field_name"])
+		case "avatar":
+			userInfo.Avatar = ldapUserInfo.GetAttributeValue(v["ldap_field_name"])
+		case "sex":
+			userInfo.Sex = ldapUserInfo.GetAttributeValue(v["ldap_field_name"])
+		case "email":
+			userInfo.Email = ldapUserInfo.GetAttributeValue(v["ldap_field_name"])
+		case "remark":
+			userInfo.Remark = ldapUserInfo.GetAttributeValue(v["ldap_field_name"])
+		}
+	}
+
+	return
+}
--- pkg/ldap/updatePwd.go
+++ pkg/ldap/updatePwd.go
@@ -1,8 +1,47 @@
 /*
-@Time : 2020/12/20 下午3:44 
+@Time : 2020/12/20 下午3:44
 @Author : hoastar
 @File : updatePwd
 @Software: GoLand
 */
 
 package ldap
+
+import (
+	"fmt"
+	"github.com/go-ldap/ldap/v3"
+	"github.com/hoastar/orange/pkg/logger"
+	"github.com/spf13/viper"
+	"golang.org/x/text/encoding/unicode"
+)
+
+func LdapUpdatePwd(username string, oldPassword string, newPassword string) (err error) {
+	err = ldapConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	var userDn = fmt.Sprintf("cn=%v,%v", username, viper.GetString("settings.ldap.baseDn"))
+
+	err = conn.Bind(userDn, oldPassword)
+	if err != nil {
+		logger.Error("用户或密码错误。", err)
+		return
+	}
+
+	sql2 := ldap.NewModifyRequest(userDn, nil)
+
+	utf16 := unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM)
+	pwdEncoded, _ := utf16.NewEncoder().String(newPassword)
+
+	sql2.Replace("unicodePwd", []string{pwdEncoded})
+	sql2.Replace("userAccountControl", []string{"512"})
+
+	if err = conn.Modify(sql2); err != nil {
+		logger.Error("密码修改失败，%v", err.Error())
+		return
+	}
+
+	return
+}
